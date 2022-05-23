6900cbc231c465c58cfee023568aea48cf3bb4e6
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-24 00:33:55 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
      <h1>Hello Bro</h1>
      <button class="btn btn-primary">Button</button>
+     <Routes>
+    <Route path='/' element={<Home></Home>}> </Route>
 
+     </Routes>
     </div>
   );
 }
