59c4722888b8398abf40b96da72eb9b49fd237ea
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-29 11:20:19 +0600

    my order deleting implimented

--- src/Pages/Components/MyOrder/MyOrder.js
+++ src/Pages/Components/MyOrder/MyOrder.js
@@ -1,14 +1,38 @@
-import React from 'react';
+// import React from 'react';
 
-const MyOrder = ({myOrder}) => {
+// const MyOrder = ({myOrder}) => {
+//     const handleOrderCancel = id => {
+//         const proceed = window.confirm('Are you sure you want to cancel?')
+//         if(proceed){
+
+//             console.log('deleting', id);
+//             const url = `http://localhost:5000/ordered-products/${id}`;
+//             fetch(url, {
+//                 method: 'DELETE'
+//             })
+//             .then(res => res.json())
+//             .then(data => {
+//                 console.log('deleted')
+//                 // if(data.deletedCount > 0){
+//                 //     console.log('deleted');
+//                 //     const remaining = myOrder.filter()
+//                 // }
+//             })
+
+//         }
+//     }
     
-    return (
-        <div>
-            <h2>Product Name: {myOrder.product}</h2>
-            <p>Ordered Quantity: {myOrder.orderQuantity}</p>
-            <button className='btn btn-primary'>Payment</button>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h2>Product Name: {myOrder.product}</h2>
+//             <p>Ordered Quantity: {myOrder.orderQuantity}</p>
 
-export default MyOrder;
+//            <div className='flex'>
+//            <button className='btn btn-primary mr-5'>Payment</button>
+//            <button onClick={ ()=> handleOrderCancel(myOrder._id)} className='btn btn-primary'>Cancel</button>
+//            </div>
+//         </div>
+//     );
+// };
+
+// export default MyOrder;
--- src/Pages/MyOrders/MyOrders.js
+++ src/Pages/MyOrders/MyOrders.js
@@ -2,34 +2,59 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import MyOrder from '../Components/MyOrder/MyOrder';
 
 const MyOrders = () => {
     const [user] = useAuthState(auth)
     const [myOrders, setMyOrders] = useState([]);
-    useEffect(() =>{
+    useEffect(() => {
         const getOrders = async () => {
             const email = user.email;
             const url = `http://localhost:5000/ordered-products?email=${email}`;
-            const {data} = await axios.get(url);
+            const { data } = await axios.get(url);
             setMyOrders(data);
 
         }
         getOrders();
-    }, [user])
-    return (
-        <div >
-             <h1  className='text-xl text-primary text-center'>Your Orders: {myOrders.length}</h1>
+    }, [user]);
+    const handleOrderCancel = id => {
+        const proceed = window.confirm('Are you sure you want to cancel?')
+        if(proceed){
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-5'>
-           
+            console.log('deleting', id);
+            const url = `http://localhost:5000/ordered-products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log('deleted')
+                if(data.deletedCount > 0){
+                    console.log('deleted');
+                    const remaining = myOrders.filter(myOrder => myOrder._id !== id);
+                    setMyOrders(remaining);
+                }
+            })
+
+        }
+    }
+    return (
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center my-5'>
             {
-                myOrders.map(myOrder => <MyOrder
-                key={myOrder._id}
-                myOrder = {myOrder}
-                ></MyOrder>)
+                myOrders.map(myOrder => <div key={myOrder._id}>
+                    <h2>Product Name: {myOrder.product}</h2>
+                    <p>Ordered Quantity: {myOrder.orderQuantity}</p>
+                    <div className='flex'>
+                        <button className='btn btn-primary mr-5'>Payment</button>
+                        <button onClick={() => handleOrderCancel(myOrder._id)} className='btn btn-primary'>Cancel</button>
+                    </div>
+
+
+
+
+
+                </div>)
             }
-        </div>
         </div>
     );
 };
